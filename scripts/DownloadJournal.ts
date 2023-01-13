8c7e4fa63e58a6672b32e78700009dadc426a877
John Nolan <me@johnnolan.dev>
2023-01-13 16:06:28 +0000

    Simplify popup and tab output

--- scripts/DownloadJournal.ts
+++ scripts/DownloadJournal.ts
@@ -1,8 +1,7 @@
 class DownloadJournal {
   public static Tab(journalHtml: string, name: string): void {
-    const bl = new Blob([journalHtml], { type: "text/html" });
     const a = document.createElement("a");
-    a.href = URL.createObjectURL(bl);
+    a.href = this._generateHtmlBlob(journalHtml);
     a.download = `${name}.html`;
     a.hidden = true;
     document.body.appendChild(a);
@@ -11,15 +10,18 @@ class DownloadJournal {
   }
 
   public static Popup(journalHtml: string, name: string): void {
-    const win = window.open(
-      "",
+    window.open(
+      this._generateHtmlBlob(journalHtml),
       name,
       "toolbar=no,location=no,directories=no,status=no,menubar=no,scrollbars=yes,resizable=yes,width=780,height=800,top=" +
         (screen.height - 400) +
         ",left=" +
         (screen.width - 840)
     );
-    win.document.body.innerHTML = journalHtml;
+  }
+
+  private static _generateHtmlBlob(journalHtml: string): string {
+    return URL.createObjectURL(new Blob([journalHtml], { type: "text/html" }));
   }
 }
 
